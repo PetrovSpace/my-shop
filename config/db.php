d6cae2966176051a19d9bdf0707d6c2ba3761572
Vladimir <vovapetrov94@gmail.com>
2022-01-30 08:21:59 +0300

    Shopping cart

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=srv11.host-food.ru;dbname=h156075_foryii2',
+    'username' => 'h156075_foryii2',
+    'password' => 'h156075_foryii2',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
