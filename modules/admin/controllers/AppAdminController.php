c06543f5498b65b0d8317c8801a7391678e5cdf7
Владимир <vovapetrov94@gmail.com>
2022-05-26 06:50:04 +0300

    Уроки 21,22,23
    Авторизация администратора. Часть 1,2,3

---
+++ modules/admin/controllers/AppAdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\modules\admin\controllers;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class AppAdminController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
